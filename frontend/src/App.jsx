4f2c4258780e4831a2638e5f0e223797ffe57ec5
Rommel Haynes <rhaynes3k@gmail.com>
2024-12-20 18:24:40 -0600

    Added Best Seller, Footer, News Letter and Policy components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Login from './pages/Login'
 import Orders from './pages/Orders'
 import PlaceOrder from './pages/PlaceOrder'
 import Product from './pages/Product'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -26,6 +27,7 @@ const App = () => {
       <Route path='/place-order' element={<PlaceOrder/>} />
       <Route path='/orders' element={<Orders/>} />
       </Routes>
+      <Footer />
     </div>
   )
 }
